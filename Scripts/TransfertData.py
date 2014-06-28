218de1b668e09b9605cfdf6d85fe531a9a76010f
John <haplo31@neuf.fr>
2014-06-28 13:30:49 -0700

    Modifs en plus, marche pas, code a verifier

---
+++ Scripts/TransfertData.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+import cgi,serial
+print "Location: http://192.168.1.13\n"
+form=cgi.FieldStorage()
+action=form.getvalue('action')
+ser=serial.Serial('/dev/ttyACM0',115200)
+ser.write(action)
+
+
+
+
