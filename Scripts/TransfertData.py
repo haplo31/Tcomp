0e2a3baede91c2c247bace1a6be954ab91c32a18
John <haplo31@neuf.fr>
2014-07-11 14:21:17 +0200

    Modif script

--- Scripts/TransfertData.py
+++ Scripts/TransfertData.py
@@ -1,12 +1,12 @@
-#!/usr/bin/python
+!/usr/bin/python
 import cgi,serial
 print "Location: http://192.168.1.13\n"
 form=cgi.FieldStorage()
 action=form.getvalue('action')
 ser=serial.Serial('/dev/ttyACM0',115200)
 ser.write(action)
-initrec='0'
-while initrec=='0' :
+initrec=0
+while initrec==0 :
 	valrec= ser.readline()
 	if valrec=="done" :
 		initrec=1
