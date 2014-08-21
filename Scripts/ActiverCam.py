381d8678ba3c2aa3f2f0474646579d1fd7efefc4
John <hapo31@neuf.fr>
2014-08-21 19:26:46 +0000

    Rajout Scripts Cam

--- Scripts/ActiverCam.py
+++ Scripts/ActiverCam.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python
 import os,sys
-os.system("/usr/bin/raspistill -o /var/www/page/test.jpg -tl 1000 -t 3600000 -w 256 -h 256")
+os.system("/usr/bin/raspistill -t 30000 -tl 1000 -q 30 -vf -w 128 -h 128 -o /var/www/page/test.jpg")
 print "Location: http://192.168.1.13\n"
---
+++ Scripts/DesactiverCam.py
@@ -0,0 +1,4 @@
+#!/usr/bin/python
+import os,sys
+os.system("pkill raspistill")
+print "Location: http://192.168.1.13\n"
